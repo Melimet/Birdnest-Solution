4f88eea9713b3dc3154b0673e4a36bf5a28ecde3
Erkka Rahikainen <erkka.rahikainen@gmail.com>
2022-12-03 20:50:52 +0200

    Clean up droneXmlParser

--- backend/src/utils/droneXmlParser.ts
+++ backend/src/utils/droneXmlParser.ts
@@ -1,31 +1,35 @@
 import { z } from 'zod'
-import { Drone, DroneZodArray } from '../types'
+import { Drone, DroneZod } from '../types'
 
-function stringExtractor(input: string, value: string) {
+function stringExtractor(input: string, value: string): string | undefined {
   const result = input.match(new RegExp(`<${value}>(.*)</${value}>`))
 
   return result?.[1]
 }
 
-function convertXmlToDrones(xml: string): Drone[] {
+function convertXmlToDrones(xml: string): Drone[] | undefined {
   const [, ...xmlInArray] = xml.split('<drone>')
 
-  const droneData = xmlInArray.map((droneInfo) => {
-    const wantedValues = ['serialNumber', 'positionY', 'positionX']
-      .map((string) => stringExtractor(droneInfo, string))
-    
-    return {
+  const droneData = xmlInArray.flatMap((droneInfo) => {
+    const wantedValues = ['serialNumber', 'positionY', 'positionX'].map(
+      (string) => stringExtractor(droneInfo, string)
+    )
+
+    const drone = {
       serialNumber: wantedValues[0],
       positionY: Number(wantedValues[1]),
       positionX: Number(wantedValues[2]),
     }
+    const parsedDrone = DroneZod.safeParse(drone)
+    return parsedDrone.success ? parsedDrone.data : []
   })
 
+  console.log(droneData)
 
-  return DroneZodArray.parse(droneData)
+  return droneData
 }
 
-function parseDroneXml(xmlInput: unknown) {
+function parseDroneXml(xmlInput: unknown): Drone[] | undefined {
   const parsed = z.string().parse(xmlInput)
   const drones = convertXmlToDrones(parsed)
 
