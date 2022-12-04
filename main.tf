3b6bb1b8e9390ca9ce603e0c3c72c48a2ce505f5
Erkka Rahikainen <erkka.rahikainen@gmail.com>
2022-12-04 18:42:52 +0200

    add secure password generation for db

--- main.tf
+++ main.tf
@@ -26,4 +26,24 @@ resource "aws_db_instance" "birdnest_rds" {
   engine_version       = "13.3"
   instance_class       = "db.t3.micro"
   username             = "melimet"
+  password             = random_password.db_master_pass.result
+}
+
+resource "random_password" "db_master_pass" {
+  length            = 40
+  special           = true
+  min_special       = 5
+  override_special  = "!#$%^&*()-_=+[]{}<>:?"
+  keepers           = {
+    pass_version  = 1
+  }
+}
+
+resource "aws_secretsmanager_secret" "db-pass" {
+  name = "db-pass-${terraform.workspace}"
+}
+
+resource "aws_secretsmanager_secret_version" "db-pass-val" {
+  secret_id     = aws_secretsmanager_secret.db-pass.id
+  secret_string = random_password.db_master_pass.result
 }
