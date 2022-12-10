e8119c9fbe1e564265afcd46ad5e4a409fca2e7f
Erkka Rahikainen <erkka.rahikainen@gmail.com>
2022-12-10 20:16:03 +0200

    Update useEffect inside custom hook

--- frontend/src/hooks/usePilotSocket.tsx
+++ frontend/src/hooks/usePilotSocket.tsx
@@ -7,10 +7,13 @@ const socket = io('http://localhost:3001/')
 function usePilotSocket() {
   const [pilots, setPilots] = useState<PilotType[]>([])
 
-  socket.on('pilots', (pilotData) => {
-    console.log('PILOT DATA ', pilotData)
-    setPilots(pilotData.pilots)
-  })
+  useEffect(() => {
+    socket.on('pilots', (pilotData) => {
+      console.log('PILOT DATA ', pilotData)
+      setPilots(pilotData.pilots)
+    })
+  }, [])
+ 
   return pilots
 }
 
