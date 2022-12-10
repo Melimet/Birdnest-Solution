46d45dd23dd2acbdbf62b4cd5d6413ebc02b537e
Erkka Rahikainen <erkka.rahikainen@gmail.com>
2022-12-10 23:20:30 +0200

    Introduce zod to frontend

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,10 +1,13 @@
-export interface PilotType {
-  id: number
-  pilotId: string
-  firstName: string
-  lastName: string
-  email: string
-  phoneNumber: string
-  distance: number
-  latestNdzBreach: number
-}
+import { z } from 'zod'
+
+export const PilotZod = z.object({
+  pilotId: z.string(),
+  firstName: z.string(),
+  lastName: z.string(),
+  phoneNumber: z.string(),
+  email: z.string(),
+  distance: z.number().positive(),
+  latestNdzBreach: z.string(),
+})
+
+export type PilotType = z.infer<typeof PilotZod>
