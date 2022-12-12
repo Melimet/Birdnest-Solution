56ac232628e641b4faa07839d0ee4ffbd17f776c
Erkka Rahikainen <erkka.rahikainen@gmail.com>
2022-12-12 21:45:04 +0200

    Add return type to drone poller

--- backend/src/services/drone/dronePoller.ts
+++ backend/src/services/drone/dronePoller.ts
@@ -2,8 +2,9 @@ import axios from 'axios'
 
 const url = 'https://assignments.reaktor.com/birdnest/drones'
 
-async function pollDroneApi() {
+async function pollDroneApi(): Promise<string> {
   const response = await axios.get(url)
+
   return response.data
 }
 
