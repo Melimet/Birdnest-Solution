3d384b2e98adb272aed56375a3a2e368da90aed9
Erkka Rahikainen <erkka.rahikainen@gmail.com>
2022-12-21 23:08:22 +0200

    Update aws router with subnet ids

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -58,6 +58,7 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route" "public" {
+  subnet_ids             = [aws_subnet.private.*.id]
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.birdnest-igw.id
