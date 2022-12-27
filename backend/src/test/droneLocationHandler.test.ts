3ffe308a3d550791d5676f80fade41bd4059b87b
Erkka Rahikainen <erkka.rahikainen@gmail.com>
2022-12-27 18:41:30 +0200

    Update tests to reflect made changes

--- backend/src/test/droneLocationHandler.test.ts
+++ backend/src/test/droneLocationHandler.test.ts
@@ -15,12 +15,13 @@ describe('handleDroneLocations', () => {
         serialNumber: 'SN-vMc3aUUffP',
         positionY: 128919.37283294523,
         positionX: 438148.3336630254,
+        latestNdzBreach: "123",
       },
     ]
 
     const results = handleDroneLocations(drones)
     const expectedResults = [
-      { serialNumber: 'SN-vMc3aUUffP', distance: 223.74 },
+      { serialNumber: 'SN-vMc3aUUffP', distance: 223.74, latestNdzBreach: "123"},
     ]
 
     expect(results).toEqual(expectedResults)
@@ -35,15 +36,15 @@ describe('checkForNDZViolations', () => {
   })
   test('should return a correct array of distances', () => {
     const distances = [
-      { serialNumber: 'SN-7IBnsWgTBI', distance: 100876.0167995431 },
-      { serialNumber: 'SN-ugj-fRr0Qm', distance: 99099.62832485135 },
-      { serialNumber: 'SN-BEcekTwQ9S', distance: 197900.95946953492 },
-      { serialNumber: 'SN-o6rP7EYERf', distance: 221864.59440413464 },
+      { serialNumber: 'SN-7IBnsWgTBI', distance: 100876.0167995431, latestNdzBreach: "123" },
+      { serialNumber: 'SN-ugj-fRr0Qm', distance: 99099.62832485135, latestNdzBreach: "123" },
+      { serialNumber: 'SN-BEcekTwQ9S', distance: 197900.95946953492, latestNdzBreach: "123" },
+      { serialNumber: 'SN-o6rP7EYERf', distance: 221864.59440413464, latestNdzBreach: "123" },
     ]
 
     const results = checkForNDZViolations(distances, 100000)
     const expectedResults = [
-      { serialNumber: 'SN-ugj-fRr0Qm', distance: 99099.62832485135 },
+      { serialNumber: 'SN-ugj-fRr0Qm', distance: 99099.62832485135, latestNdzBreach: "123" },
     ]
 
     expect(results).toEqual(expectedResults)
--- backend/src/test/test-resources/droneDistanceData.ts
+++ backend/src/test/test-resources/droneDistanceData.ts
@@ -1,8 +1,8 @@
 export const validDroneDistanceData = [
-  { serialNumber: 'SN-EG1s8i5-hd', distance: 80988.96549972756 },
+  { serialNumber: 'SN-EG1s8i5-hd', distance: 80988.96549972756, latestNdzBreach: "123"},
 ]
 
 export const invalidDroneDistanceData = [
-  { serialNumber: 'Ssrjgisdgkjsbdgdsjbkfbsd', distance: 80988.96549972756 },
-  { serialNumber: 'SN-EVHbrvfaesdasdasd2§3123m', distance: 58456.74565855545 },
+  { serialNumber: 'Ssrjgisdgkjsbdgdsjbkfbsd', distance: 80988.96549972756, latestNdzBreach: "123" },
+  { serialNumber: 'SN-EVHbrvfaesdasdasd2§3123m', distance: 58456.74565855545, latestNdzBreach: "123" },
 ]
