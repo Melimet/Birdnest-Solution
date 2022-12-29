5f924442f5c08feaef6fde90716ec5fb8e671f52
Erkka Rahikainen <erkka.rahikainen@gmail.com>
2022-12-29 17:22:28 +0200

    Update security groups for load balancer

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -6,6 +6,26 @@ resource "aws_security_group" "birdnest_ecs_security_group" {
     from_port = 0
     to_port = 0
     protocol = "-1"
+    security_groups = [aws_security_group.birdnest_load_balancer_security_group.id]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+  
+resource "aws_security_group" "birdnest_load_balancer_security_group" {
+  vpc_id      = aws_vpc.birdnest-vpc.id
+  name        = "birdnest-load-balancer-security-group"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  
     ipv6_cidr_blocks = ["::/0"]
   }
@@ -17,9 +37,4 @@ resource "aws_security_group" "birdnest_ecs_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
-  tags = {
-    "name" = "birdnest-ecs-security-group"
-  }
 }
-  
