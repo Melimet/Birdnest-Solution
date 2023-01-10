6165846db9d9c957346cdab1e711036b48f95619
Erkka Rahikainen <erkka.rahikainen@gmail.com>
2023-01-10 21:42:07 +0200

    Create security group for rds

--- terraform/db.tf
+++ terraform/db.tf
@@ -8,6 +8,7 @@ resource "aws_db_instance" "birdnest_rds" {
   username             = "melimet"
   password             = random_password.db_master_pass.result
   publicly_accessible    = true
+  security_group_names = [aws_security_group.birdnest_rds_security_group.id]
 }
 
 resource "random_password" "db_master_pass" {
